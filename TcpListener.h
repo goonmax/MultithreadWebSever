d435cdc9a628a4bdb451028fae31d83511769197
chetynan <chetynan@hotmail.com>
2019-11-01 20:48:00 +1100

    got boost/beast working woohoo! plus added main

--- TcpListener.h
+++ TcpListener.h
@@ -2,6 +2,19 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <boost/aligned_storage.hpp>
+#include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/asio.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+
+
+
+
 
 class TcpListener
 {
