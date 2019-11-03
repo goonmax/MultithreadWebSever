e565c1f3b47055a946a593119acd0f4cf160ac31
chetynan <chetynan@hotmail.com>
2019-11-03 11:11:51 +1100

    update read me and started adding some thing sto websocket cpp file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MultithreadWebSever
 working progress cheeho
+
+> This is using boost and im going to be trying to use and utilize performance, haven't quite fleashed out what the end product will look like.
+> Hoping this will be a good exercise to teach me what a real world project with concurrency is like to deal with.
--- websocket_huddle.cpp
+++ websocket_huddle.cpp
@@ -1,1 +1,15 @@
 #include "websocket_huddle.hpp"
+#include <iostream>
+
+
+websocket_huddle::websocket_huddle(tcp::socket && socket, boost::shard_ptr<mutex> const & state) : ws_(std::move(socket)), state_(state)
+{
+
+}
+
+websocket_huddle::~websocket_huddle()
+{
+	// gets rid of the connection on the active sessions
+	state_->leave(this);
+}
+
